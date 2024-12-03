e2faebe4744cea3c3d5e6fd3f486c9fcd8646a66
Yanjin Chen <yanjin.chen@student.tdsb.on.ca>
2024-12-03 10:24:59 -0500

    Restarted.

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,37 +1,15 @@
 import './Feature.css';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Genres from "../components/Genres.jsx"
 
 function Featured() {
-    const [movies, setMovies] = useState([]);
-    /*const navigate = useNavigate();*/
-
-    useEffect(() => {
-        (async function getMovies() {
-            const response = await axios.get(
-                 `https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`
-            );
-            setMovies(response.data.results);
-        });
-    });
-
-    function loadMovie(id) {
-        navigate(`/movies/${id}`);
-    }
-
+    
     return (
-        <div>
-            <h2>Featured Movies</h2>
-            <div className="movies-container">
-                {movies.map((movie) => (
-                    <div key={movie.id} className="movie-card">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                            alt={movie.title}
-                            className="movie-poster"
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
+      <div>
+        
+      </div>
     );
 }
 
