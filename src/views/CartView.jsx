977c687ff97975afa9929ac77807f567cbcf5eac
ychen <yanjincchen@gmail.com>
2024-12-15 00:30:04 -0500

    Fixed issue with poster images not rendering properly, remove movie works and also updates the detailed view button with add to cart.

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -1,37 +1,38 @@
-//Create a view called CartView.jsx that can be accessed via /cart URL. Each movie in the
-//cart should have its trailer image, title and a button to remove it.
 import { useNavigate } from "react-router-dom";
 import { useStoreContext } from "../context/GlobalState";
 import "./CartView.css";
 
 function CartView() {
-    const { 
-        cartItems, setCartItems, 
-        firstName
-    } = useStoreContext();
+    const { cartItems, setCartItems, firstName } = useStoreContext();
 
     function remove(movie) {
-        setCart(cart.filter(item => item !== movie));
+        setCartItems(cartItems.filter(item => item !== movie)); // Correct the cartItems state update
     }
+
+    console.log(cartItems);
 
     return (
         <div className="cart-view">
             <h2>{firstName}'s Cart</h2>
-            <div className="cart-item-container">
-                {cartItems.map((movie) => (
-                    <div key={movie.id} className="cart-view-item">
-                        <p>{movie.title}</p>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                            alt={movie.title}
-                            className="cart-view-image"
-                        />
-                        <button className="trash" onClick={() => { remove(movie)}}>
-                            Delete item
-                        </button>
-                    </div>
-                ))}
-            </div>
+            {cartItems.length === 0 ? (
+                <p className="empty-cart-message">Your cart is empty. Add some movies!</p>
+            ) : (
+                <div className="cart-item-container">
+                    {cartItems.map((movie) => (
+                        <div key={movie.id} className="cart-view-item">
+                            <p>{movie.title}</p>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster}`}
+                                alt={movie.title}
+                                className="cart-view-image"
+                            />
+                            <button className="trash" onClick={() => { remove(movie) }}>
+                                Delete item
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
